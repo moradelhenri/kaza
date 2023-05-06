b1948e5f18bbf9bd25fe409d2bfc2715a53db3d0
moradelhenri <83418498+moradelhenri@users.noreply.github.com>
2023-05-06 21:39:38 +0200

    code source build

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -12,7 +12,7 @@ return (
     <div className='iconStyle'>
 {ratingStart.map(( index) => 
 
-elem >= index ? (<img key={index} className='icon-style' src ={starActive} />  ) :  ( < img key={index} className='icon-style' src= {starInactive}  />
+elem >= index ? (<img key={index} className='icon-style' src ={starActive}  alt='etoile pleine'/>  ) :  ( < img key={index} className='icon-style' src= {starInactive} alt='etoile vide' />
 )  
 )
   
